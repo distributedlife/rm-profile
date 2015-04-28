59cc8640bd625dff2b784ecf7549b3acfdbd5b29
Ryan Boucher <ryan.boucher@distributedlife.com>
2015-04-28 14:42:59 +1000

    compressed svgs; resized images; added width and height; other pagespeed boons; added deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+s3cmd sync --acl-public --guess-mime-type --recursive --rexclude '^\.' --rexclude '\/\.' . s3://rm-profile/
+
+s3cmd modify --acl-public --guess-mime-type --add-header="Cache-Control:max-age=86400" --add-header="Content-Encoding: gzip" s3://rm-profile/*.svg
+
+s3cmd modify --acl-public --guess-mime-type --add-header="Cache-Control:max-age=86400" s3://rm-profile/*.png
+
+s3cmd modify --acl-public --guess-mime-type --add-header="Cache-Control:max-age=86400" s3://rm-profile/*.jpg
+
+s3cmd modify --acl-public --guess-mime-type --add-header="Content-Type: utf-8" s3://rm-profile/index.html
